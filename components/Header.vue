927cccc0946f2e48777a2a9c0bcf39609e3a12cd
teyz <contact@bastienrigaud.fr>
2022-07-28 10:47:10 +0200

    project init

--- components/Header.vue
+++ components/Header.vue
@@ -139,6 +139,7 @@ header {
     animation: smoothScroll 0.5s forwards;
     position: fixed;
     background-color: $primary;
+    box-shadow: 0px 3px 6px #00000006;
 
     .logoMobile {
       cursor: pointer;
